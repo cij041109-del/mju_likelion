f4be994dbf4937d4c11a388aaf9ff4292359e5b3
인준 <cij041109@gmail.com>
2026-04-07 16:31:03 +0900

    for/while/do-while

---
+++ src/week1/likelion/Loops.java
@@ -0,0 +1,29 @@
+package week1.likelion;
+
+public class Loops {
+
+    public static void main(String[] args) {
+
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        //while
+
+        int b = 10;
+        int c= 10;
+
+        while(b > 0) {
+            System.out.println(b);
+            b--;
+        }
+
+        //do-while문
+
+        do {
+            System.out.println(c);
+            c--;
+        }while(c > 0);
+    }
+}
