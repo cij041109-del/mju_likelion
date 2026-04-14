928cbb6c224765fb1fc0b43815c2a62d420d34b7
인준 <cij041109@gmail.com>
2026-04-14 12:52:44 +0900

    fix: Step3 접근 제어자 테스트 코드 수정 및 메시지 보완

--- src/week2/task2/step3.java
+++ src/week2/task2/step3.java
@@ -13,12 +13,12 @@ public class step3 {
         System.out.println("👤 이름: " + lion.name + " | 📚 전공: " + lion.getMajor() + "| 📌 기수: " + lion.getNum());
 
         System.out.println("\n📌 Step 3-1. public 필드 접근을 시도합니다.");
-        System.out.println("major 필드 값을 변경합니다.");
+        System.out.println("name 필드 값을 변경합니다.");
         lion.name = "홍길동";
         System.out.println("🦁 아기사자 정보를 출력합니다.");
         System.out.println("👤 이름: " + lion.name + " | 📚 전공: " + lion.getMajor() + "| 📌 기수: " + lion.getNum());
 
-/*      //dafault 접근 불가. 주석을 풀면 에러.
+/*      // default 접근 불가 (다른 패키지이므로). 주석 해제 시 컴파일 오류 발생
         System.out.println("\n📌 Step 3-2. default 필드 접근을 시도합니다.");
         System.out.println("name 필드 값을 변경합니다.");
         lion.major = "기계공학과";
@@ -30,7 +30,7 @@ public class step3 {
     /*    //private 접근 불가. 주석을 풀면 에러.
         System.out.println("\n📌 Step 3-3. private 필드 접근을 시도합니다.");
         System.out.println("num 필드 값을 변경합니다.");
-        lion.num = "10";
+        lion.num = 10;
         System.out.println("🦁 아기사자 정보를 출력합니다.");
         System.out.println("👤 이름: " + lion.name + " | 📚 전공: " + lion.getMajor() + "| 📌 기수: " + lion.getNum());
     */
